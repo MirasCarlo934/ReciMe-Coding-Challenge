429213530ab41c3efa32c177a29dff2cb6dfae29
Carlo Miras <mirascarlo.business@gmail.com>
2025-07-29 21:30:41 +0800

    Add skeletal SpringBootApplication with basic RestController

---
+++ src/main/java/com/recime/codingchallenge/RecimeCodingChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.recime.codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecimeCodingChallengeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecimeCodingChallengeApplication.class, args);
+    }
+}
+
